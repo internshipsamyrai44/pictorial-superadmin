51b95e0d98bfdab0b44ddf3e096235acc76a65e9
dyv21 <denis@artkerr.com>
2025-04-19 22:26:37 +0300

    feat: add Header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,16 +1,15 @@
-'use client'
-
-import s from './Header.module.css'
-import logoIcon from '../../../public/icons/Inctagram.svg'
-import Image from 'next/image'
-import {SelectTranslate} from "@/widgets/header/selectTranslate/SelectTranslate"
-import Link from "next/link"
+'use client';
+import s from './Header.module.css';
+import logoIcon from '../../../public/icons/Inctagram.svg';
+import Image from 'next/image';
+import { SelectTranslate } from '@/widgets/header/selectTranslate/SelectTranslate';
+import Link from 'next/link';
 
 export const Header = () => (
   <header className={s.header}>
     <Link href={'/'}>
-      <Image src={logoIcon} alt="Inctagram"/>
+      <Image src={logoIcon} alt="Inctagram" />
     </Link>
     <SelectTranslate />
   </header>
-)
+);
