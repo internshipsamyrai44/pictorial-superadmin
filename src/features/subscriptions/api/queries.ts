e2c51c775ded98bb27079ae41220a6f5146906dc
dyv21 <denis@artkerr.com>
2025-04-25 15:02:04 +0300

    feat: add Following

--- src/features/subscriptions/api/queries.ts
+++ src/features/subscriptions/api/queries.ts
@@ -22,3 +22,26 @@ export const GET_USER_FOLLOWERS = gql`
     }
   }
 `;
+
+export const GET_USER_FOLLOWING = gql`
+  query getFollowing($userId: Int!, $pageSize: Int, $pageNumber: Int, $sortBy: String, $sortDirection: SortDirection) {
+    getFollowing(
+      userId: $userId
+      pageSize: $pageSize
+      pageNumber: $pageNumber
+      sortBy: $sortBy
+      sortDirection: $sortDirection
+    ) {
+      pagesCount
+      page
+      pageSize
+      totalCount
+      items {
+        id
+        userId
+        userName
+        createdAt
+      }
+    }
+  }
+`;
