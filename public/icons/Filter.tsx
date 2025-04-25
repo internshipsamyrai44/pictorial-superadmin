9d5674dfb1c16d02745cc4dc8b75b147bc42dc55
metedri <mitedria@gmail.com>
2025-04-25 16:22:46 +0300

    feat: payment sorting added

---
+++ public/icons/Filter.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Filter = () => {
+  return (
+    <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 -1.52588e-05L7.4641 4.49998H0.535898L4 -1.52588e-05Z" fill="#4C4C4C" />
+      <path d="M4 12L0.535898 7.49998L7.4641 7.49998L4 12Z" fill="#4C4C4C" />
+    </svg>
+  );
+};
+
+export default Filter;
