915fac903041b30b2d2705ee3f88f7a688dacd48
alexandr-obraztsov <alexlove2005@mail.ru>
2025-05-04 09:36:46 +0300

    feat: add user ban indicator in user table

--- src/pages/users/ui/user-table/user-row/UserRow.tsx
+++ src/pages/users/ui/user-table/user-row/UserRow.tsx
@@ -4,7 +4,7 @@ import { User } from '@/entities/user/types';
 import { UserActionsMenu } from './UserActionsMenu';
 import { BanModal } from './ui/BanModal';
 import { useState } from 'react';
-
+import Block from 'public/icons/block.svg';
 type UserRowProps = {
   user: User;
 };
@@ -19,7 +19,12 @@ export const UserRow = ({ user }: UserRowProps) => {
   return (
     <>
       <tr>
-        <td>{user.id}</td>
+        <td>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+            {user.userBan ? <Block /> : <div style={{ width: '24px', height: '24px' }} />}
+            {user.id}
+          </div>
+        </td>
         <td>
           {user.profile.firstName} {user.profile.lastName}
         </td>
