2365a1ba7cfbd76e11a2c0885100bd8ed80f4eab
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-05-17 11:03:11 +0300

    style(table): fix styles for sort arrow

--- public/icons/Filter.tsx
+++ public/icons/Filter.tsx
@@ -6,18 +6,22 @@ type FilterProps = {
 
 const Filter = ({ sortDir }: FilterProps) => {
   const inactiveColor = "#4C4C4C";
-  const activeColor = "#397DF6";
+  const activeColor = "currentColor";
 
   return (
     <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path 
-        d="M4 -1.52588e-05L7.4641 4.49998H0.535898L4 -1.52588e-05Z" 
-        fill={sortDir === 'asc' ? activeColor : inactiveColor} 
-      />
-      <path 
-        d="M4 12L0.535898 7.49998L7.4641 7.49998L4 12Z" 
-        fill={sortDir === 'desc' ? activeColor : inactiveColor} 
-      />
+      {(sortDir === 'asc' || sortDir === null) && (
+        <path 
+          d="M4 -1.52588e-05L7.4641 4.49998H0.535898L4 -1.52588e-05Z" 
+          fill={sortDir === 'asc' ? activeColor : inactiveColor} 
+        />
+      )}
+      {(sortDir === 'desc' || sortDir === null) && (
+        <path 
+          d="M4 12L0.535898 7.49998L7.4641 7.49998L4 12Z" 
+          fill={sortDir === 'desc' ? activeColor : inactiveColor} 
+        />
+      )}
     </svg>
   );
 };
